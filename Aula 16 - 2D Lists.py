401f543f73659876fcf44b8e9440ac00377aea5d
ThalesAbdon <thales.sousufpi@gmail.com>
2023-10-20 18:36:13 -0300

    estudando listas 2d

---
+++ Aula 16 - 2D Lists.py
@@ -0,0 +1,7 @@
+adcarrys = ["Lucian", "Jinx", "Kai'sa"]
+suports = ["Soraka", "Braum", "Leona"]
+jungles = ["Rammus", "Zac", "Graves"]
+
+champions = [adcarrys, suports, jungles]
+
+print(champions)
