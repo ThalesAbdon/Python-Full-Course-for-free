4894ae7f772609520b75528ceb4209a8aa4f0652
ThalesAbdon <thales.sousufpi@gmail.com>
2023-10-20 18:47:13 -0300

    Estudando tuplas

---
+++ Aula 17 - Tuple.py
@@ -0,0 +1,12 @@
+# Tuplas são uma sequência de objetos imutáveis, em outras palavras, uma vez criadas,
+# tuplas não podem ser modificadas, normalmente são usadas para guardar dados protegidos.
+# As tuplas são escritas entre parênteses ().
+
+linguagens = ("Python", "Ruby", "Javascript", "Perl", "Haskell")
+print(linguagens)
+
+print(linguagens[0:3])
+print(linguagens[-1])
+print(linguagens[:-2])
+print(linguagens[:])
+print(len(linguagens))
